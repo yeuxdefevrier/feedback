fb39426fea049cd774757d3e3b129cbc16c41b76
Leano <s2401340@edu.bc.fi>
2026-03-17 12:23:44 +0200

    feedback

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "templates"));
+
+app.use("/tyylit", express.static("includes/styles"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = 3000;
+const host = "localhost";
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/feedback", (req, res) => {
+  res.render("feedback");
+});
+
+app.post("/feedback", (req, res) => {
+  const name = req.body.name;
+  const feedback = req.body.feedback;
+  const email = req.body.email;
+  res.render("thank you", { name: name, email: email });
+});
+
+app.listen(port, host, () => console.log(`${host}:${port} listen...`));
